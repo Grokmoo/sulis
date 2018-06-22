7239ea24bb9899b462bde27a67e1bf4b175d8314
Jared Stephen <grok_moo@yahoo.com>
2018-06-22 17:10:28 -0400

    ability ap cleanup

--- data/scripts/powerful_blows.lua
+++ data/scripts/powerful_blows.lua
@@ -5,9 +5,15 @@ function on_activate(parent, ability)
 
   effect = parent:create_effect(ability:name())
   effect:deactivate_with(ability)
-  effect:add_damage(2, 5, 5)
   effect:add_num_bonus("accuracy", 10)
   effect:add_num_bonus("defense", -10)
+  
+  if parent:has_ability("devastating_blows") then
+    effect:add_damage(3, 8, 5)
+	effect:add_num_bonus("crit_multiplier", 0.5)
+  else
+    effect:add_damage(2, 5, 0)
+  end
 
   gen = parent:create_anim("crossed_swords")
   gen:set_moves_with_parent()

8c9fe5bf033424670d18fd4b049bf2e8b9738e41
Jared Stephen <grok_moo@yahoo.com>
2018-07-01 23:20:11 -0400

    arrow storm ability

---
+++ data/scripts/arrow_storm.lua
@@ -0,0 +1,20 @@
+max_dist = 30
+
+function on_activate(parent, ability)
+  targets = parent:targets():without_self()
+  
+  targeter = parent:create_targeter(ability)
+  targeter:set_free_select(max_dist * 2)
+  targeter:set_shape_cone(parent:center_x(), parent:center_y(), max_dist, math.pi / 8) 
+  targeter:add_all_effectable(targets)
+  targeter:activate()
+end
+
+function on_target_select(parent, ability, targets)
+  targets = targets:to_table()
+  for i = 1, #targets do
+    parent:anim_weapon_attack(targets[i])
+  end
+
+  ability:activate(parent)
+end

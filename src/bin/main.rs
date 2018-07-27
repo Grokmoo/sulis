4557465ec86790449a002938b91b8bfebc7a11b0
Jared Stephen <grok_moo@yahoo.com>
2018-07-27 17:37:21 -0400

    don't spawn console window

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,6 +14,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with Sulis.  If not, see <http://www.gnu.org/licenses/>
 
+#![windows_subsystem = "windows"]
+
 extern crate sulis_core;
 extern crate sulis_module;
 extern crate sulis_state;

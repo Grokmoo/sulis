5714fe65d0af6cfd999caa79971a69d2484e523f
Jared Stephen <grok_moo@yahoo.com>
2020-05-16 18:58:20 -0700

    endless dungeon fixes and balancing

--- campaigns/endless_dungeon/scripts/moonmouth.lua
+++ campaigns/endless_dungeon/scripts/moonmouth.lua
@@ -1,3 +1,7 @@
 function on_area_load(parent)
   game:start_conversation("start", parent)
 end
+
+function on_boss_end(parent)
+  game:show_game_over_window("Congratulations on completing the Endless Dungeon and thanks for playing!")
+end

5a8235f7fab92bf73e21608d4e31f035d17f502c
Jared Stephen <grok_moo@yahoo.com>
2020-09-14 22:37:36 -0700

    naathfir dwarves

--- campaigns/twin_expanse_act3/scripts/naathfir.lua
+++ campaigns/twin_expanse_act3/scripts/naathfir.lua
@@ -12,3 +12,16 @@ end
 function start_dwarves_quest(parent)
   game:set_quest_entry_state("naathfir_dwarves", "start", "Visible")
 end
+
+function merch_rest(parent)
+  game:run_script_delayed("campaign", "fire_rest", 0.0)
+end
+
+function dwarves_planned(parent)
+  game:set_quest_entry_state("naathfir_dwarves", "planned", "Visible")
+  game:set_quest_entry_state("the_aegis", "counterattack", "Visible")
+  game:add_party_item("dwarven_signal_rune")
+  game:enable_prop_at(72, 46)
+  game:set_world_map_location_visible("volgar_river", true)
+  game:set_world_map_location_enabled("volgar_river", true)
+end

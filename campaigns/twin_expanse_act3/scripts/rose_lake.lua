2a87d42db436bdca4bdaa669622e2edcfe1269d2
Jared Stephen <grok_moo@yahoo.com>
2020-11-27 10:32:40 -0800

    rose lake campaign content

--- campaigns/twin_expanse_act3/scripts/rose_lake.lua
+++ campaigns/twin_expanse_act3/scripts/rose_lake.lua
@@ -6,12 +6,25 @@ function pull_lever(parent)
   game:set_quest_entry_state("naathfir_dwarves", "the_attack", "Visible")
   game:set_quest_state("naathfir_dwarves", "Complete")
   
-  game:spawn_actor_at("councilor_berkeley", 110, 75, "Neutral", "rose_lake")
+  local entity = game:spawn_actor_at("councilor_berkeley", 110, 75, "Neutral", "rose_lake")
+  
+  game:player():set_flag("rose_lake_berkeley_id", entity:id())
   
   game:spawn_actor_at("dwarf_raider01", 125, 68, "Neutral", "rose_lake")
   game:spawn_actor_at("dwarf_raider02", 125, 74, "Neutral", "rose_lake")
 end
 
 function on_area_load(parent)
+  local berkeley_id = game:player():get_flag("rose_lake_berkeley_id")
+  if berkeley_id == nil then return end
+
+  local berkeley = game:entity_with_id(berkeley_id)
+  if not berkeley:is_valid() then return end
   
+  game:start_conversation("rose_lake_berkeley", berkeley)
+end
+
+function to_xandala(parent)
+  game:set_quest_entry_state("the_aegis", "to_xandala", "Visible")
+  game:transition_party_to(120, 120, "xandala")
 end
